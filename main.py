9e4709af2593c36414ba9d75da2460b6c2adb469
Steve Mings <stevennmings@gmail.com>
2017-10-21 00:40:34 -0500

    All works to 1/2 video except adding to database

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from flask import Flask, request, redirect, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 
-
 app = Flask(__name__)
 app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:password@localhost:8889/build-a-blog'
@@ -23,12 +22,14 @@ class Blog(db.Model):
 @app.route('/blog', methods=['POST', 'GET'])
 def blog_page():
 
-    if request.method == 'POST':
+    if ['POST'] == True:
+                
         title_name = request.form['title']
         body_name = request.form['body']
         new_entry = Blog(title_name, body_name)
         db.session.add(new_entry)
         db.session.commit()
+
 
     blogs = Blog.query.all()
     
@@ -37,11 +38,34 @@ def blog_page():
 
 @app.route('/newpost', methods=['POST', 'GET'])
 def new_post():
-    return render_template('newpost.html', title = "New Post!")
+    return render_template('newpost.html', titlebase = 'New Blog Post!')
+
+
+@app.route('/validate_newpost', methods=['POST', 'GET'])
+def validate_newpost():
+    title = request.form['title']
+    body = request.form['body']
+
+    title_error=''
+    body_error=''
+
+#title
+    if len(title) == 0:
+        title_error = "You Must Enter a Title!"
+
+# body
+    if len(body) == 0:
+        body_error = "You Must Enter A Body!"
+
+    if not title_error and not body_error:
+        return redirect('/blog')
+    else:
+        return render_template('newpost.html', title_error=title_error, body_error=body_error)
+
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
-    return render_template('blog.html', title="Its a Blog!")
+    return redirect('/blog')
 
 
 if __name__ == '__main__':
